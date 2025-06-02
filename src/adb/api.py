f7e2de0387bec1e07bc1321d0f6713f0055d43eb
helittr <1139686772@qq.com>
2025-06-02 22:01:17 +0800

    "增强 ADB 脚本管理功能：添加脚本执行状态与日志查询接口，优化参数验证与错误处理，重构日志存储路径，并新增 PowerShell 脚本示例"

--- src/adb/api.py
+++ src/adb/api.py
@@ -1,9 +1,14 @@
-from fastapi import APIRouter, Path
-from . import scriptManager as smgr
-from .scriptManager import ExecutParam
+"""
+api scriptmanager
+"""
+
 import pathlib
 from typing import Annotated
-import logging
+# import logging
+
+from fastapi import APIRouter, Path, Response, Query
+from . import script_manager as smgr
+from .script_manager import ExecuteParam
 
 
 router = APIRouter(prefix="/adb", tags=["ADB"])
@@ -18,15 +23,36 @@ def get_commands():
     """获取ADB命令列表"""
     return manager.rootgroup
 
-@router.post("/commands/{id}/execute", summary="执行指定命令")
+
+@router.post("/commands/{sid}/execute", summary="执行指定命令")
 def exe_command(
-    id: Annotated[int, Path(title="The ID of the command to execute")],
-    params: ExecutParam,
+    sid: Annotated[int, Path(title="The ID of the command to execute")],
+    params: ExecuteParam,
 ):
-    print(f"excute command{id}",params)
+    """the api to execute script"""
+    print(f"excute command{sid}", params)
     try:
-        manager.execute_script(id, params)
+        manager.execute_script(sid, params)
+        return {"status": "ok", "code": 0, "execution_id": sid}
     except ValueError as e:
         print("exception", e)
-        return {"status": "ok", "code": 0, "message": str(e)}
-    return {"status": "ok", "code": 0}
+        return {"status": "error", "code": 1, "message": str(e)}
+
+
+@router.get("/commands/{sid}/status", summary="获取指定命令状态")
+def get_status(sid: Annotated[int, Path(title="The ID of the command to get")]):
+    """get command status"""
+    st = manager.get_script_status(sid)
+    return {"id": sid, "code": st}
+
+
+@router.get("/commands/{sid}/log", summary="获取指定命令日志")
+def get_log(
+    sid: Annotated[int, Path(title="The ID of the command to get")],
+    pos: int = Query(default=0, ge=0, title="The position of the log to get"),
+    size: int = Query(default=-1, title="The size of the log to get"),
+):
+    """get command log"""
+    log = manager.get_script_log(sid, pos, size)
+    print("response log", log.decode("gb2312", errors="ignore"))
+    return Response(content=log, media_type="application/octet-stream")
